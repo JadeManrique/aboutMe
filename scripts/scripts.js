45a4afabfb12023df78f3311df9aa8aa2ba55c55
Jade Manrique <jade.a.manrique@gmail.com>
2018-04-24 09:48:30 -0700

    Fixed .css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+  $(".hov").hover(function() {
+    $(this).css("color","red");
+    $(this).fadeOut("slow", function() {
+      $(this).fadeIn("slow");
+    });
+  });
 
+  $(".hov2").hover(function() {
+    $(this).animate({fontSize: '25px'}, 500);
+  });
+
+  $("h1").hover(function() {
+    $(this).animate({fontSize: '4em'}, 600);
+  });
 
 });
